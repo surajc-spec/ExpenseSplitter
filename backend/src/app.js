5660b371feed4b055f60a424dbc88aedd58c7f60
surajc-spec <surajchougule378@gmail.com>
2026-09-09 00:23:50 +0530

    testing push

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-
+hello
 app.use("/api/auth", authRoutes);
 
 app.get("/api/health", async (req, res) => {
